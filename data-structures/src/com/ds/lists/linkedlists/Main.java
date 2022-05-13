d3e55eec02c1566f8b4827e59b75d0f8ba994e00
Israel Mesa <mesa.israel50@gmail.com>
2022-05-13 15:52:47 -0400

    linkedlists

---
+++ data-structures/src/com/ds/lists/linkedlists/Main.java
@@ -0,0 +1,31 @@
+package com.ds.lists.linkedlists;
+
+import com.ds.lists.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new  Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 321);
+        Employee marySmith =  new Employee("Mary", "Smith", 456);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 987);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        System.out.println(list.isEmpty());
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.getSize());
+
+        list.removeFromFront();
+
+        list.printList();
+
+        System.out.println(list.getSize());
+    }
+
+}
