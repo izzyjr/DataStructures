72d4cbd9f91ec71b66c30b73301ef8f4836e6f5b
Israel Mesa <mesa.israel50@gmail.com>
2022-06-06 12:56:07 -0400

    sorting algorithms

--- data-structures/src/com/ds/arrays/Main.java
+++ data-structures/src/com/ds/arrays/Main.java
@@ -1,5 +1,12 @@
 package com.ds.arrays;
 
+import static com.ds.sort.Sort.bubbleSort;
+import static com.ds.sort.Sort.countingSort;
+import static com.ds.sort.Sort.insertionSort;
+import static com.ds.sort.Sort.quickSort;
+import static com.ds.sort.Sort.selectionSort;
+import static com.ds.sort.Sort.shellSort;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,150 +23,13 @@ public class Main {
 
         int[] intArray2 = {2, 5, 9, 103, 2, 8, 7, 10, 4, 100};
 
-//        bubbleSort(intArray);
-//        selectionSort(intArray);
-//        insertionSort(intArray);
-//        shellSort(intArray);
+        bubbleSort(intArray);
+        selectionSort(intArray);
+        insertionSort(intArray);
+        shellSort(intArray);
         quickSort(intArray, 0, 7);
-
         countingSort(intArray2, 1, 105);
 
         System.out.println(java.util.Arrays.toString(intArray2));
-
-    }
-
-    public static void bubbleSort(int[] array) {
-
-        int length = array.length;
-        int temp = 0;
-
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                if (array[i] < array[j]) {
-                    temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
-            }
-        }
-    }
-
-    public static void selectionSort(int[] array) {
-
-        int temp = 0;
-
-        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            int largest = 0;
-            for (int j = 1; j <= lastUnsortedIndex; j++) {
-                if (array[j] > array[largest]) {
-                    largest = j;
-                }
-            }
-            temp = array[lastUnsortedIndex];
-            array[lastUnsortedIndex] = array[largest];
-            array[largest] = temp;
-        }
-    }
-
-    public static void insertionSort(int[] array) {
-
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
-            int newElement = array[firstUnsortedIndex];
-            int i;
-            for (i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
-                array[i] = array[i - 1];
-            }
-            array[i] = newElement;
-        }
-    }
-
-    public static void shellSort(int[] array) {
-
-        for (int gap = array.length / 2; gap > 0; gap/= 2) {
-            for (int i = gap; i < array.length; i++) {
-                int newElement = array[i];
-
-                int j = i;
-
-                while (j >= gap && array[j - gap] > newElement) {
-                    array[j] = array[j - gap];
-                    j -= gap;
-                }
-                array[j] = newElement;
-            }
-        }
-    }
-
-    public static int iterativeFactorial(int num) {
-
-        if (num == 0) {
-            return 1;
-        }
-
-        int factorial = 1;
-        for (int i = 1; i <= num; i++) {
-            factorial *= i;
-        }
-
-        return factorial;
-    }
-
-    public static int recursiveFactorial(int num) {
-
-        if (num == 0) {
-            return 1;
-        }
-
-        return num * recursiveFactorial(num - 1);
-    }
-
-    public static void quickSort(int[] array, int start, int end) {
-
-        if (end - start < 2) {
-            return;
-        }
-
-        int pivotIndex = partition(array, start, end);
-        quickSort(array, start, pivotIndex);
-        quickSort(array, pivotIndex + 1, end);
-    }
-
-    private static int partition(int[] array, int start, int end) {
-
-        int pivot = array[start];
-        int left = start;
-        int right = end;
-
-        while (left < right) {
-
-            while (left < right && pivot <= array[--right]);
-            if (left < right) {
-                array[left] = array[right];
-            }
-
-            while (left < right && pivot >= array[++left]);
-            if (left < right) {
-                array[right] = array[left];
-            }
-        }
-        array[left] = pivot;
-        return left;
-    }
-
-    public static void countingSort(int[] array, int min, int max) {
-
-        int[] countArray = new int[(max - min) + 1];
-
-        for (int i = 0; i < array.length; i++) {
-            countArray[array[i] - min]++;
-        }
-
-        int j = 0;
-        for (int i = min; i <= max; i++) {
-            while (countArray[i - min] > 0) {
-                array[j++] = i;
-                countArray[i - min]--;
-            }
-        }
     }
 }
---
+++ data-structures/src/com/ds/sort/Sort.java
@@ -0,0 +1,139 @@
+package com.ds.sort;
+
+public class Sort {
+
+    public static void bubbleSort(int[] array) {
+
+        int length = array.length;
+        int temp = 0;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (array[i] < array[j]) {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] array) {
+
+        int temp = 0;
+
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for (int j = 1; j <= lastUnsortedIndex; j++) {
+                if (array[j] > array[largest]) {
+                    largest = j;
+                }
+            }
+            temp = array[lastUnsortedIndex];
+            array[lastUnsortedIndex] = array[largest];
+            array[largest] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
+            int newElement = array[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
+                array[i] = array[i - 1];
+            }
+            array[i] = newElement;
+        }
+    }
+
+    public static void shellSort(int[] array) {
+
+        for (int gap = array.length / 2; gap > 0; gap/= 2) {
+            for (int i = gap; i < array.length; i++) {
+                int newElement = array[i];
+
+                int j = i;
+
+                while (j >= gap && array[j - gap] > newElement) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = newElement;
+            }
+        }
+    }
+
+    public static int iterativeFactorial(int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    public static int recursiveFactorial(int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * recursiveFactorial(num - 1);
+    }
+
+    public static void quickSort(int[] array, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(array, start, end);
+        quickSort(array, start, pivotIndex);
+        quickSort(array, pivotIndex + 1, end);
+    }
+
+    private static int partition(int[] array, int start, int end) {
+
+        int pivot = array[start];
+        int left = start;
+        int right = end;
+
+        while (left < right) {
+
+            while (left < right && pivot <= array[--right]);
+            if (left < right) {
+                array[left] = array[right];
+            }
+
+            while (left < right && pivot >= array[++left]);
+            if (left < right) {
+                array[right] = array[left];
+            }
+        }
+        array[left] = pivot;
+        return left;
+    }
+
+    public static void countingSort(int[] array, int min, int max) {
+
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            countArray[array[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                array[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+    }
+}
